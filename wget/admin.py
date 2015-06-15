a5a54f77b865ed753b0530b4ef06c967dc29f171
Choi Kirin <iizs@naver.com>
2015-06-15 06:11:09 +0000

    admin UI for wget module

--- wget/admin.py
+++ wget/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import Channel, PersistentQueue
+
+admin.site.register(Channel)     
+admin.site.register(PersistentQueue)     
--- wget/models.py
+++ wget/models.py
@@ -6,8 +6,15 @@ class Channel(models.Model):
     count_in = models.IntegerField(default=0)
     count_out = models.IntegerField(default=0)
 
+    def __unicode__(self):
+        return self.channel_id
+
 class PersistentQueue(models.Model):
     channel = models.ForeignKey('Channel')
     datetime = models.DateTimeField(auto_now=True, auto_now_add=True, db_index=True)
     body = models.TextField()
 
+    def __unicode__(self):
+        return str(self.channel) + ' / ' + str(self.datetime)
+
+QUEUE_FETCH_URL = 'wget.urls_to_fetch'
