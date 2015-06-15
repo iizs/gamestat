4260a4faf14f4673c6aaf6a6f1731dca2fd3cf4e
Choi Kirin <iizs@naver.com>
2015-06-15 11:28:56 +0000

    rules added to admin

--- wget/admin.py
+++ wget/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from models import Channel, PersistentQueue
+from models import Channel, PersistentQueue, PostProcessRule
 
 admin.site.register(Channel)     
 admin.site.register(PersistentQueue)     
+admin.site.register(PostProcessRule)     
