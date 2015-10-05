d6efd1ae6c3f145ab9942725466914f4e896228b
Choi Kirin <iizs@naver.com>
2015-10-05 05:32:15 +0000

    Fix some standings on beginning day missing

--- kbo/management/commands/kbomakestandings.py
+++ kbo/management/commands/kbomakestandings.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Count
 
 from optparse import make_option
 
@@ -62,6 +63,8 @@ class Command(BaseCommand):
         ).delete()
 
         standings = {}
+
+        # find latest standings of this season
         s = Standing.objects.filter(
                 date__gte = season.start_date,
                 date__lt = startdate,
@@ -69,6 +72,7 @@ class Command(BaseCommand):
         ).order_by('-date')[0:1]
 
         if len(s) != 0:
+            # if found fill 'standings' dict
             l = Standing.objects.filter(
                     date = s[0].date,
                     season = season,
@@ -76,6 +80,30 @@ class Command(BaseCommand):
             for i in l:
                 standings[i.team] = i
             startdate = s[0].date + datetime.timedelta(days=1)
+        else :
+            # if not found fill standings dict with empty standing objects
+            for t in Score.objects.filter(
+                        date__gte = season.start_date,
+                        date__lte = season.end_date,
+                    ).values('home_team').annotate(Count('home_team')):
+
+                o = Standing(
+                    season = season,
+                    date = startdate, 
+                    team = t['home_team'],
+                    l10 = '',
+                    games = 0, 
+                    wins = 0,
+                    losses = 0, 
+                    draws = 0, 
+                    pct = 0, 
+                    gb = 0, 
+                    streak = 0, 
+                    home_wins = 0,
+                    home_losses = 0, 
+                    home_draws = 0, 
+                )
+                standings[o.team] = o
 
         c_date = startdate;
         while c_date <= enddate:
